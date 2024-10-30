fc13377e9827218747f100e37e521b24e6142c11
sink666 <er.basilisk@gmail.com>
2024-10-30 00:11:24 +0000

    rewriting. put things in classes

--- mapgen.py
+++ mapgen.py
@@ -1,81 +1,87 @@
-import array
 import random
 
-IMG_W = 100
-IMG_H = 100
-PPM_HEADER = f'P6 {IMG_W} {IMG_H} {255} '
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
-image = array.array('B')
-field = []
+WALL = 0
+SPACE = 1
 
-def noise() -> int:
-    r = random.randrange(1, 100)
-    if r > 35:
-        return 1
-    else:
-        return 0
+class Generator:
+    def __init__(self, f_width, f_height, weight) -> None:
+        self.width = f_width
+        self.height = f_height
+        self.rand_weight = weight
+        self.field = []
 
+    def __get_in_field(self, x, y):
+        return self.field[x + (y * self.width)]
+    
+    def __should_be_wall(self, x, y) -> bool:
+        acc = 0
 
-def field_to_img():
-    image.clear()
+        if x == 0 or x == (self.width - 1) or y == 0 or y == (self.height - 1):
+            return False
 
-    for p in field:
-        if p == 0:
-            image.fromlist(BLACK)
-        else:
-            image.fromlist(WHITE)
-
-
-def should_be_wall(x, y) -> bool:
-    acc = 0
-
-    if x == 0 or x == (IMG_W - 1) or y == 0 or y == (IMG_H - 1):
-        return True
-
-    for yp in (-1, 0, 1):
-        for xp in (-1, 0, 1):
-            if field[(x + xp) + ((y + yp) * IMG_W)] == 0:
-                acc += 1
+        for yp in (-1, 0, 1):
+            for xp in (-1, 0, 1):
+                if self.__get_in_field(x + xp, y + yp) == WALL:
+                    acc += 1
         
-    if acc >= 4:
-        return True
-    else:
-        return False
+        return True if acc >= 5 else False
+        
+    def iterate_world(self):
+        newfield = []
+
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.__should_be_wall( x, y):
+                    newfield.append(WALL)
+                else:
+                    newfield.append(SPACE)
+
+        self.field[:] = newfield
 
 
 
-def iterate_world():
-    newfield = []
+class Output:
+    def __init__(self) -> None:
+        self.img_num = 0
+        self.image = []
+        
+    def field_to_img(self, field):
+        self.image.clear()
 
-    for y in range(IMG_H):
-        for x in range(IMG_W):
-            if should_be_wall(x, y) == True:
-                newfield.append(0)
+        for p in field:
+            if p == WALL:
+                self.image.extend(BLACK)
             else:
-                newfield.append(1)
+                self.image.extend(WHITE)
 
-    field[:] = newfield
+    def put_file(self, width, height):
+        filename = "test" + str(self.img_num) + ".ppm"
 
+        with open(filename, 'wb') as f:
+            f.write(bytearray(f'P6 {width} {height} {255} ', 'ascii'))
+            f.write(bytes(self.image))
 
-def out_to_file(file_num):
-    filename = "test" + str(file_num) + ".ppm"
+        self.img_num += 1
 
-    with open(filename, 'wb') as f:
-        f.write(bytearray(PPM_HEADER, 'ascii'))
-        image.tofile(f)
 
 
 def main():
     random.seed()
+    gen = Generator(100, 100, 50)
+    out = Output()
 
-    for _ in range(IMG_W * IMG_H):
-        field.append(noise())
+    for _ in range(gen.width * gen.height):
+        if random.randrange(1, 100) < gen.rand_weight:
+            gen.field.append(WALL)
+        else:
+            gen.field.append(SPACE)
 
-    for fv in range(9):
-        iterate_world()
-        field_to_img()
-        out_to_file(fv)
+    for _ in range(20):
+        gen.iterate_world()
+        out.field_to_img(gen.field)
+        out.put_file(gen.width, gen.height)
 
 
 if __name__ == '__main__':
