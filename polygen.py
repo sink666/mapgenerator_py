4672f4a65362f2cc9fb0dd9648cd980f249e31ec
sink666 <er.basilisk@gmail.com>
2024-11-02 03:08:02 +0000

    remove this class. why did i do this

--- polygen.py
+++ polygen.py
@@ -1,41 +1,27 @@
 import random
 
 # black, white, red, blue, green, yellow, cyan, magenta
-PALETTE = [
-    [0, 0, 0],
-    [255, 255, 255],
-    [255, 0, 0],
-    [0, 255, 0],
-    [0, 0, 255],
-    [255, 255, 0],
-    [0, 255, 255],
-    [255, 0, 255]
-]
+PALETTE = ['000000','FFFFFF','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF']
 
-class Output:
-    def __init__(self) -> None:
-        self.img_num = 0
-        self.image = []
-        
-    def data_to_img(self, data):
-        self.image.clear()
+def put_file(width, height, data):
+    filename = "test.ppm"
+
+    with open(filename, 'wb') as f:
+        f.write(bytearray(f'P6 {width} {height} {255} ', 'ascii'))
 
         for p in data:
-            self.image.extend(PALETTE[p])
-
-    def put_file(self, width, height):
-        filename = "test" + str(self.img_num) + ".ppm"
-
-        with open(filename, 'wb') as f:
-            f.write(bytearray(f'P6 {width} {height} {255} ', 'ascii'))
-            f.write(bytes(self.image))
-
-        self.img_num += 1
-
+            f.write(bytes.fromhex(PALETTE[p]))
 
 
 def main():
-    random.seed()
+    # random.seed()
+    img = [ 
+        0, 1, 2, 
+        3, 4, 5, 
+        6, 7, 0, 
+    ]
+
+    put_file(3, 3, img)
 
 
 
